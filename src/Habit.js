a5a2e3e0578bb6c30fb6e1a6cbd0446f6b614527
marckimbrey <marckimbrey@gmail.com>
2020-10-27 07:55:45 +0000

    highlight habit  checks onClick

--- src/Habit.js
+++ src/Habit.js
@@ -5,21 +5,44 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 class Habit extends React.Component {
   constructor(props) {
     super(props);
+    const selected = this.props.dates.filter((date) => {
+      return this.props.habit.days.indexOf(date) !== -1;
+    });
+    console.log(selected);
+    this.state = { selected: selected };
   }
+  updateSelected(date) {
+    const selectedDates = this.state.selected.slice();
+    const index = selectedDates.indexOf(date);
+    if (index !== -1) {
+      selectedDates.splice(index, 1);
+    } else {
+      selectedDates.push(date);
+    }
+    this.setState({ selected: selectedDates });
+  }
+
   render() {
-    console.log(this.props);
     return (
       <tr className="habit">
         <td className="habit-title">{this.props.habit.name}</td>
 
         {this.props.dates.map((date, i) => {
+          let color = "#666";
+          if (this.state.selected.indexOf(date) !== -1) {
+            color = "#fc3";
+          }
           return (
             <td key={i}>
               <FontAwesomeIcon
                 className="habit-check"
                 icon={faCheck}
                 key={date}
-                onClick={() => this.props.checkDay(this.props.habit.name, date)}
+                style={{ color: color }}
+                onClick={() => {
+                  this.props.checkDay(this.props.habit.name, date);
+                  this.updateSelected(date);
+                }}
               />
             </td>
           );
