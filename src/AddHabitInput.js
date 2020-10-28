25d86fe64dabfb88f08b17e34e0951f7a5d72328
marckimbrey <marckimbrey@gmail.com>
2020-10-28 12:37:42 +0000

    Removed AddHabitButton component and adjusted styles to Header component

--- src/AddHabitInput.js
+++ src/AddHabitInput.js
@@ -3,19 +3,22 @@ import React from "react";
 function AddHabitInput(props) {
   //const closeHabitBtn = props.isMobile === 60? <div className="add-habit-close" onClick={props.toggleInput}></div>: '';
   return (
-    <form className="add-habit">
+    <form
+      className="add-habit"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const input = document.querySelector(".add-habit-input");
+        props.addHabit(input.value);
+      }}
+    >
       <label for="add-habit">add habit</label>
-      <input className="add-habit-input" name="add-habit" type="text" />
-      <button
-        type="submit"
-        onClick={(e) => {
-          e.preventDefault();
-          const input = document.querySelector(".add-habit-input");
-          props.addHabit(input.value);
-        }}
-      >
-        submit
-      </button>
+      <input
+        className="add-habit-input"
+        name="add-habit"
+        type="text"
+        placeholder="New Habit"
+      />
+      <button type="submit">submit</button>
     </form>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,52 +1,15 @@
 import React from "react";
-import AddHabitButton from "./AddHabitButton";
 import AddHabitInput from "./AddHabitInput";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputActive: false,
-      width: 60,
-    };
-  }
-
-  // function to control animation
-  toggleInput(e) {
-    let width = this.state.width === 60 ? window.innerWidth * 0.9 : 60;
-    this.updateInputActive(600);
-    this.setState({ width: width });
-  }
-
-  updateInputActive(delay) {
-    setTimeout(() => {
-      this.setState({ inputActive: !this.state.inputActive });
-    }, delay);
-  }
-
-  render() {
-    const active = this.state.inputActive;
-    let input;
-    if (active || window.innerWidth > 600) {
-      // if button clicked or large screen
-      input = (
-        <AddHabitInput
-          addHabit={this.props.addHabit}
-          toggleInput={(e) => this.toggleInput(e)}
-          isMobile={this.state.width}
-        />
-      );
-    } else {
-      input = (
-        <AddHabitButton
-          width={this.state.width}
-          toggleInput={(e) => this.toggleInput(e)}
-        />
-      );
-    }
-
-    return <div className="App-header">{input}</div>;
-  }
+function Header(props) {
+  return (
+    <div className="App-header">
+      <h1 className="title">
+        HABIT<span>TRACKER</span>
+      </h1>
+      <AddHabitInput addHabit={props.addHabit} />
+    </div>
+  );
 }
 
 export default Header;
