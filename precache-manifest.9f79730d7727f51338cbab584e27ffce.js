c813d77db1a9474be7ff1ddfc17d06b5e3e18f6c
marckimbrey <marckimbrey@gmail.com>
2020-10-29 09:24:53 +0000

    Updates

---
+++ precache-manifest.9f79730d7727f51338cbab584e27ffce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69e8d33929c530b7ebb5b92d556be0c",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "ba6f33d0000b31d90655",
+    "url": "/habit-tracker/static/css/main.a5a680cc.chunk.css"
+  },
+  {
+    "revision": "5074dcdff278d3272e30",
+    "url": "/habit-tracker/static/js/2.8a4f6db0.chunk.js"
+  },
+  {
+    "revision": "5f2d6de39c39f8b6e46315c29574aa22",
+    "url": "/habit-tracker/static/js/2.8a4f6db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba6f33d0000b31d90655",
+    "url": "/habit-tracker/static/js/main.1d3a18d3.chunk.js"
+  },
+  {
+    "revision": "41cca30c9e06b96923bc",
+    "url": "/habit-tracker/static/js/runtime-main.582c2f91.js"
+  }
+]);
